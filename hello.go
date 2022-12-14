1d5ab04f47965d346bf9160ca3edce586aabcddb
Ryan Soto <ryansotodev@gmail.com>
2022-12-13 22:38:01 -0800

    renamed file to reference learning topic, created binary file to execute

---
+++ hello.go
@@ -0,0 +1,10 @@
+//Go programs are organized in packages. Main package is the entry point of program, also identifies an executable program.
+package main
+
+//fmt is a built-in go package. provides input/output functions.
+import "fmt"
+
+//where program starts, as mentioned above.
+func main() {
+	fmt.Println("Hello, world!")
+}
