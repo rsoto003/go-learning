693cdbe86cdd99434fd7fdf3b7592e5e314cb6e1
Ryan Soto <ryansotodev@gmail.com>
2022-12-21 01:02:30 -0800

    created helloHandler func

--- go-server/main.go
+++ go-server/main.go
@@ -6,6 +6,33 @@ import (
 	"net/http"
 )
 
-func main() {
-	fmt.Println("hello world")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
 }
+
+func formHandler() {
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
